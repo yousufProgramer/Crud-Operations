9d55b104a85970f78055afdd9cc76ef8f7e06a5d
yousuf <yousufahmed27093@gmail.com>
2022-12-28 21:11:40 +0600

    added redirect page to homePage

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from './component/Home/Home';
-import UserProvider from './component/UserProvider';
-import { Delete } from './component/Delete';
-import { Read } from './Read/Read';
-import { Create } from './component/Create/Create';
-import Edit from './component/Edit/Edit';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Home } from "./component/Home/Home";
+import UserProvider from "./component/UserProvider";
+import { Delete } from "./component/Delete";
+import { Read } from "./Read/Read";
+import { Create } from "./component/Create/Create";
+import Edit from "./component/Edit/Edit";
 function App() {
   return (
     <div className="App">
       <UserProvider>
-         <BrowserRouter>
-      <Routes>
-      <Route path="/edit/:id" element={<Edit />}>
-        </Route>
-
-      <Route path="/create" element={<Create />}>
-        </Route>
-      <Route path="/read/:id" element={<Read />}>
-        </Route>
-        <Route path="/" element={<Home />}>
-        </Route>
-        <Route path="/delete/:id" element={<Delete />}>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-    </UserProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/create" element={<Create />}></Route>
+            <Route path="/read/:id" element={<Read />}></Route>
+            <Route path="/edit/:id" element={<Edit />}></Route>
+            <Route path="/delete/:id" element={<Delete />}></Route>
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </BrowserRouter>
+      </UserProvider>
     </div>
   );
 }
